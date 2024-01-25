f8d96e4bd8b78e2e9ded4135616f99db5f7e5557
shubhamsawant18 <shubhsawant124@gmail.com>
2024-01-25 23:43:17 +0530

    uselocation Hook2

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,25 +4,9 @@ import React, { createContext, useState } from 'react';
 const NoteContext = createContext();
 
 const NoteState = (props) => {
-  const s1 = {
-    name: 'Anaira',
-    class: 'first',
-  };
-
-  const [state, setstate] = useState(s1);
-
-  const update = () => {
-    setTimeout(() => {
-      setstate((prevState) => ({
-        ...prevState,
-        name: 'Larry',
-        class: '10b',
-      }));
-    }, 1000);
-  };
-
+ 
   return (
-    <NoteContext.Provider value={{ state: state, update: update }}>
+    <NoteContext.Provider value={{ }}>
       {props.children}
     </NoteContext.Provider>
   );
